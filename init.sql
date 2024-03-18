8c1326ac92ffd994bb5587b014580ecb752a6984
nemanjaonex2 <nemanjaonex@gmail.com>
2024-03-18 11:59:51 +0100

    fix volume

--- init.sql
+++ init.sql
@@ -1,2 +1,2 @@
-CREATE USER 'dev'@'localhost' IDENTIFIED BY 'development';
+CREATE USER 'dev'@'localhost' IDENTIFIED BY 'password';
 GRANT ALL PRIVILEGES ON *.* TO 'dev'@'localhost' WITH GRANT OPTION;
